8cef3651130653e901aaba124bf026d83103d7be
Uriel Carneiro <urielhk17@gmail.com>
2021-12-03 14:05:42 -0300

    change next-config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
 module.exports = {
   reactStrictMode: true,
   basePath: "/checkout",
-  async rewrites() {
-    return [
-      // {
-      //   source: "/",
-      //   destination: "/",
-      // },
-      {
-        source: "https://nizen.com.br/checkout",
-        destination: "https://checkout.nizen.com.br/checkout",
-        basePath: false,
-      },
-      // {
-      //   source: "/checkout/:path*",
-      //   destination: "https://checkout.nizen.com.br/checkout/:path*",
-      // },
-    ];
+  async rewrites(props) {
+    console.log(props);
+    return {
+      fallback: [
+        {
+          source: "/",
+          destination: "https://checkout.nizen.com.br/checkout/",
+        },
+      ],
+    };
+    // return [
+    //   // {
+    //   //   source: "/",
+    //   //   destination: "/",
+    //   // },
+    //   {
+    //     source: "https://nizen.com.br/checkout",
+    //     destination: "https://checkout.nizen.com.br/checkout",
+    //     basePath: false,
+    //   },
+    //   // {
+    //   //   source: "/checkout/:path*",
+    //   //   destination: "https://checkout.nizen.com.br/checkout/:path*",
+    //   // },
+    // ];
   },
 };
