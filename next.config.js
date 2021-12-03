2ff4f0fcab3cdf5a5540fea143e68d42d76c3755
Uriel Carneiro <urielhk17@gmail.com>
2021-12-03 10:37:05 -0300

    change source

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: "/checkout",
   async rewrites() {
     return [
       {
         source: "https://www.nizen.com.br/checkout",
-        destination: `https://checkout.nizen.com.br/checkout`,
+        destination: "https://checkout.nizen.com.br/",
       },
     ];
   },
