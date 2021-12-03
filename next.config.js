6789e1913bea67a1b0646b4a2bc8738971883a9c
Uriel Carneiro <urielhk17@gmail.com>
2021-12-03 10:25:47 -0300

    added rewrites

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   basePath: "/checkout",
+  async rewrites() {
+    return [
+      {
+        source: "/checkout",
+        destination: `https://checkout.nizen.com.br/checkout`,
+      },
+    ];
+  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Home.module.css";
 import { Card } from "@ashkur/teste.ui.card";
+import { Button } from "../components/button";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         description="Card shared with bit component same used on Home MFE"
         imageUrl="https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1561&q=80"
       />
+      {/* <Button text="botao top" /> */}
     </div>
   );
 }
