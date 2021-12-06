91d269d30f0398b6b5640970d7b16698bc2f4652
Uriel Carneiro <urielhk17@gmail.com>
2021-12-06 15:01:22 -0300

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  // async rewrites(props) {
-  //   return {
-  //     fallback: [
-  //       {
-  //         source: "/",
-  //         destination: "https://checkout.nizen.com.br/checkout",
-  //       },
-  //     ],
-  //   };
-  //   // return [
-  //   //   // {
-  //   //   //   source: "/",
-  //   //   //   destination: "/",
-  //   //   // },
-  //   //   {
-  //   //     source: "https://nizen.com.br/checkout",
-  //   //     destination: "https://checkout.nizen.com.br/checkout",
-  //   //     basePath: false,
-  //   //   },
-  //   //   // {
-  //   //   //   source: "/checkout/:path*",
-  //   //   //   destination: "https://checkout.nizen.com.br/checkout/:path*",
-  //   //   // },
-  //   // ];
-  // },
+  basePath: "/checkout",
 };
