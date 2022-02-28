8a500f5a2156d15635eeec77a7a387e962d472cf
Uriel Carneiro <urielhk17@gmail.com>
2022-02-28 16:12:37 -0300

    added teste component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import styles from "../styles/Home.module.css";
 
+import { Teste } from "@evob/evob.ui.teste/dist/teste";
+
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <h1>MICRO-FRONTEND CHECKOUT</h1>
+        <Teste>
+          Hello World!
+        </Teste>
       </div>
     </>
   );
