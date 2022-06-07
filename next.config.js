f2904798a5b473cf26ef337f05bbee6c71aafa0d
Uriel Carneiro <urielhk17@gmail.com>
2022-06-07 14:00:03 -0300

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -2,27 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : '',
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/",
-          destination: "https://nizen.com.br/",
-        },
-        {
-          source: "/:path",
-          destination: "https://nizen.com.br/:path",
-        },
-        {
-          source: "/about",
-          destination: "https://nizen.com.br/about",
-        },
-        {
-          source: "/about/:path",
-          destination: "https://nizen.com.br/about/:path",
-        },
-      ],
-    };
-  },
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : ''
 };
