432e09a92eb22f8549df6abcaa61e364e73614b2
Uriel Carneiro <urielhk17@gmail.com>
2022-06-07 13:26:48 -0300

    added asset prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_PREFIX : '',
   async rewrites() {
     return {
       fallback: [
