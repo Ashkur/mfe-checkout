be4b67a89c6b7bb1a23e44e93d0701b92c2c3776
Uriel Carneiro <urielhk17@gmail.com>
2022-06-08 18:08:10 -0300

    rewrite

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,24 +6,20 @@ export function middleware(req) {
 
     console.log('has /_next/ ', req.url.includes('/_next/'))
 
-    req.nextUrl.host = 'checkout.nizen.com.br'
+    // req.nextUrl.host = 'checkout.nizen.com.br'
 
     console.log(req.nextUrl)
 
-    // if (
-    //     process.env.NODE_ENV === 'production' &&
-    //     nextUrl.href.includes('/slug/')
-    //   ) {
-    //     // nextUrl.href = `https://${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
-    //     // console.log(nextUrl.href)
-    //     // nextUrl.href = `http://${host}${nextUrl.href}`
-    //     // nextUrl.host = 'localhost:3004'
-    //     // nextUrl.hostname = 'localhost'
-    //     // nextUrl.host = 'checkout.ninizen.com.br'
-    //     // nextUrl.hostname = 'checkout.ninizen.com.br'
-
+    if (
+        process.env.NODE_ENV === 'production') {
+        // nextUrl.href = `https://${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
+        // console.log(nextUrl.href)
+        // nextUrl.href = `http://${host}${nextUrl.href}`
+        // nextUrl.host = 'localhost:3004'
+        // nextUrl.hostname = 'localhost'
+        // nextUrl.host = 'checkout.ninizen.com.br'
+        // nextUrl.hostname = 'checkout.ninizen.com.br'
         
-        
-    //     return NextResponse.rewrite(req.nextUrl.href.replace('/slug/', 'checkout.nizen.com.br/slug/'))
-    //   }
+        return NextResponse.rewrite(req.nextUrl.href.replace('/slug/', 'https://checkout.nizen.com.br/slug/'))
+      }
 }
