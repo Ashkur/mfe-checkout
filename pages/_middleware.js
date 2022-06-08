705bf4b73e95ce7a6ece69f59a2ed494778f2d1d
Uriel Carneiro <urielhk17@gmail.com>
2022-06-08 14:25:36 -0300

    added protocol

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -8,18 +8,26 @@ export function middleware(req) {
 //     );]
     let nextUrl = req.nextUrl
 
+    let url = {...req.nextUrl}._url
+
+    const host = url.host
+
     console.log({ 
         nextUrl: nextUrl, 
         href: `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`,
         pathname: nextUrl.pathname,
+        url: url,
+        host
     })
 
     if (
-        // process.env.NODE_ENV === 'production' &&
+        process.env.NODE_ENV === 'production' &&
         !nextUrl.pathname.includes('.') && // exclude all files in the public folder
         !nextUrl.pathname.startsWith('/api') // exclude all API routes
       ) {
-        nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
+        nextUrl.href = `https://${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
+        // nextUrl.href = `http://${host}${nextUrl.href}`
+        
         return NextResponse.rewrite(nextUrl)
       }
 }
