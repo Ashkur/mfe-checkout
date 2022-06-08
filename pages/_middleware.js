9bb48f4a792fc5545b06d3df2deb3f39a051a470
Uriel Carneiro <urielhk17@gmail.com>
2022-06-08 14:14:28 -0300

    pass nexturl

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -14,19 +14,12 @@ export function middleware(req) {
         pathname: nextUrl.pathname,
     })
 
-    // if(process.env.NODE_ENV === 'production') {
-    //     req.nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}/${req.nextUrl.href}`
-    //     console.log( req.nextUrl.href)
-    //     return NextResponse.rewrite(req.nextUrl.href);
-    // }
-
     if (
-        process.env.NODE_ENV === 'production' &&
+        // process.env.NODE_ENV === 'production' &&
         !nextUrl.pathname.includes('.') && // exclude all files in the public folder
         !nextUrl.pathname.startsWith('/api') // exclude all API routes
       ) {
         nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
-        // url.pathname = `/_sites/${currentHost}${url.pathname}`
-        return NextResponse.rewrite(nextUrl.href)
+        return NextResponse.rewrite(nextUrl)
       }
 }
