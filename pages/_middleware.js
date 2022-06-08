28da78a1a978bcbf3ecb8411457a8e217597bb08
Uriel Carneiro <urielhk17@gmail.com>
2022-06-08 14:11:13 -0300

    rewrite hostname

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,17 +1,32 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
+
 //   if (req.nextUrl.href.includes('/myprefix/_next/'))
 //     return NextResponse.rewrite(
 //       req.nextUrl.href.replace('/myprefix/_next/', '/_next/'),
-//     );
-    console.log(req.nextUrl)
+//     );]
+    let nextUrl = req.nextUrl
 
-    if(process.env.NODE_ENV === 'production') {
-        req.nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}/${req.nextUrl.href}`
-        console.log( req.nextUrl.href)
-        return NextResponse.rewrite(req.nextUrl.href);
-    }
+    console.log({ 
+        nextUrl: nextUrl, 
+        href: `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`,
+        pathname: nextUrl.pathname,
+    })
 
-    return null;
+    // if(process.env.NODE_ENV === 'production') {
+    //     req.nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}/${req.nextUrl.href}`
+    //     console.log( req.nextUrl.href)
+    //     return NextResponse.rewrite(req.nextUrl.href);
+    // }
+
+    if (
+        process.env.NODE_ENV === 'production' &&
+        !nextUrl.pathname.includes('.') && // exclude all files in the public folder
+        !nextUrl.pathname.startsWith('/api') // exclude all API routes
+      ) {
+        nextUrl.href = `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
+        // url.pathname = `/_sites/${currentHost}${url.pathname}`
+        return NextResponse.rewrite(nextUrl.href)
+      }
 }
