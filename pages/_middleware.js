9d5a437097bbaaa8204d9c25095fcb3fbdbab6fc
Uriel Carneiro <urielhk17@gmail.com>
2022-06-09 10:22:23 -0300

    header

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,27 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    
-    if(console.debug) console.debug(req)
-
-    console.log('has /_next/ ', req.url.includes('/_next/'))
-
-    // req.nextUrl.host = 'checkout.nizen.com.br'
-
-    // console.log(req.nextUrl.setHeader('host', 'checkout.nizen.com.br'))
-    // const a = {...req.nextUrl}
-    // console.log(a)
-
-    // if (
-    //     process.env.NODE_ENV === 'production') {
-    //     // nextUrl.href = `https://${process.env.NEXT_PUBLIC_ASSET_PREFIX}${nextUrl.href}`
-    //     // console.log(nextUrl.href)
-    //     // nextUrl.href = `http://${host}${nextUrl.href}`
-    //     // nextUrl.host = 'localhost:3004'
-    //     // nextUrl.hostname = 'localhost'
-    //     // nextUrl.host = 'checkout.ninizen.com.br'
-    //     // nextUrl.hostname = 'checkout.ninizen.com.br'
-        
-    //     return NextResponse.rewrite(req.nextUrl.href.replace('/slug/', '/slug/'))
-    //   }
+    req.headers.host = 'checkout.nizen.com.br';
+    return NextResponse.next()
 }
